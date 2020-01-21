91bebdd236688e45723daf2e2e89bed852a52c22
confident-token-1019 <theoonetj@gmail.com>
2020-01-21 02:50:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,17 +3,28 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-
-board = ["",""," "," ","X"," "," "," ",""]
-
-def position_taken?(board, index)
- 
-
-   taken = nil
-  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-     taken = false
-  else
-     taken = true
-   end
-   
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
   end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
+end
